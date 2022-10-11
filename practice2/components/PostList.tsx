28f7cec7a1decdae592951a438118eb2c84412d9
JSC <gbs04087@gmail.com>
2022-10-11 15:32:55 +0900

    FEAT: useRouter change to Link tag

--- practice2/components/PostList.tsx
+++ practice2/components/PostList.tsx
@@ -1,5 +1,6 @@
 import { AttributesType } from '@common/frontMatter';
 import { getFileList } from '@common/fs';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { title } from 'process';
 import { useEffect } from 'react';
@@ -9,24 +10,23 @@ type PostListProps = {
 };
 
 const PostList = ({ postList }: PostListProps) => {
-  const router = useRouter();
-
-  const onClickHandler = (slug: string) => {
-    router.push(slug);
-  };
   return (
     <div>
       {postList.map((e, idx) => {
         console.log(idx, e);
         return (
-          <div key={idx} onClick={() => onClickHandler(e.slug)}>
-            <div>{`제목 : ${e.title}`}</div>
-            <div>`description ${e.description}`</div>
-            {e.tags.map((tag) => (
-              <span> {tag} </span>
-            ))}
-            <div>{`date : ${e.date}`}</div>
-          </div>
+          <Link href={`/${e.slug}`}>
+            <a>
+              <div key={idx}>
+                <div>{`제목 : ${e.title}`}</div>
+                <div>`description ${e.description}`</div>
+                {e.tags.map((tag) => (
+                  <span> {tag} </span>
+                ))}
+                <div>{`date : ${e.date}`}</div>
+              </div>
+            </a>
+          </Link>
         );
       })}
     </div>
