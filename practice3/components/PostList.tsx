d4b6934e14bd28dbd7fa5779c6490c4ff3dd534a
JSC <gbs04087@gmail.com>
2022-10-15 11:48:19 +0900

    FEAT: add Link a

--- practice3/components/PostList.tsx
+++ practice3/components/PostList.tsx
@@ -16,16 +16,18 @@ const PostList = ({ category, postList }: PostListProps) => {
       {postList.map((e, idx) => {
         return (
           <Link key={idx} href={`/${category}/${e.slug}`}>
-            <Box mt={1} mb={2} p={3} sx={{ border: 1, borderRadius: '15px', margin: '10px' }}>
-              <Box>{`제목 : ${e.title}`}</Box>
-              <Box>`설명 ${e.description}`</Box>
-              <Stack direction="row" justifyContent="flex-start" spacing={1}>
-                {e.tags.map((tag) => (
-                  <Chip key={tag} label={tag} size="small" />
-                ))}
-              </Stack>
-              <Box textAlign={'end'}>{`날짜 : ${e.date}`}</Box>
-            </Box>
+            <a>
+              <Box mt={1} mb={2} p={3} sx={{ border: 1, borderRadius: '15px', margin: '10px' }}>
+                <Box>{`제목 : ${e.title}`}</Box>
+                <Box>`설명 ${e.description}`</Box>
+                <Stack direction="row" justifyContent="flex-start" spacing={1}>
+                  {e.tags.map((tag) => (
+                    <Chip key={tag} label={tag} size="small" />
+                  ))}
+                </Stack>
+                <Box textAlign={'end'}>{`날짜 : ${e.date}`}</Box>
+              </Box>
+            </a>
           </Link>
         );
       })}
